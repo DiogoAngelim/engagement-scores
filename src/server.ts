5a98c82e68e0e6276388ff1c7e3720fd74ee8336
Diogo <diogoangelim@gmail.com>
2025-12-24 08:15:59 -0300

    install mongoose

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,7 @@ mongoose.connect(MONGODB_URI)
     });
   })
   .catch((err) => {
-    console.error('MongoDB connection error:', err);
-    process.exit(1);
-  });
+    .catch ((err: unknown) => {
+      console.error('MongoDB connection error:', err);
+      process.exit(1);
+    });
